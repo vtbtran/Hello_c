7fc80c89b5fa86fd9c160fa7b36af0201538bf07
baotran <vothibaotran01012005@gmail.com>
2023-09-26 10:00:37 +0700

    flower

--- hello/main.cpp
+++ hello/main.cpp
@@ -1,32 +1,22 @@
-//
-//  main.cpp
-//  hello
-//
-//  Created by Võ Thị Bảo Trân on 23/09/2023.
-//
-
 #include <stdio.h>
 int main()
 {
-       //nhap 1 thang cho biet so ngay cua thang do
-       int thang;
-       printf("Nhap thang vao: ");
-       scanf("%d",&thang);
-       switch(thang)
+       float doanhso, hoahong;
+       printf("Nhap doanh so:");
+       scanf("%f",&doanhso);
+       if(doanhso<=100)
        {
-              case 1: printf("31 ngay"); break;
-              case 2: printf("28 ngay"); break;
-              case 3: printf("31 ngay"); break;
-              case 4: printf("30 ngay"); break;
-              case 5: printf("31 ngay"); break;
-              case 6: printf("30 ngay"); break;
-              case 7: printf("31 ngay"); break;
-              case 8: printf("31 ngay"); break;
-              case 9: printf("30 ngay"); break;
-              case 10: printf("31 ngay"); break;
-              case 11: printf("30 ngay"); break;
-              case 12: printf("31 ngay"); break;
-              default: printf("khong ton tai\n"); break;
-               
+              hoahong=0.05*doanhso;
        }
+       else if(doanhso<=300)
+       {
+              hoahong=0.1*doanhso;
+       }
+       else if(doanhso>300)
+       {
+              hoahong=0.2*doanhso;
+       }
+       printf("Hoa hong la: %f\n",hoahong);
+
 }
+
